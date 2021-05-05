532af7d7a4d2e9937be13f9da4ddc6b8b26d58ed
Jonatas Prates <jonatasluisprates@gmail.com>
2021-05-05 20:57:37 -0300

    adicionando tipos de hhtp GET - PUT - POST - DELETE

--- src/main/java/com/example/carros/api/IndexController.java
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -1,6 +1,9 @@
 package com.example.carros.api;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	
 	@GetMapping
-	public String hello() {
-		return "Hello spring boot";
+	public String get() {
+		return "get spring boot";
 	}
 	
-	@GetMapping("/teste")
-	public String testeGet() {
-		return "testando rotas";
+	@PostMapping
+	public String post() {
+		return "post spring boot";
+	}
+	
+	@PutMapping
+	public String put() {
+		return "put spring boot";
+	}
+	
+	@DeleteMapping
+	public String delete() {
+		return "delete spring boot";
 	}
 
 }

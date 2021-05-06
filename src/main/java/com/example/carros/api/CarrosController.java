92d10c2818f74d4924428fdf047b455ac0ddf47a
Jonatas Luis Prates <jonatasluisprates@gmail.com>
2021-05-06 17:33:47 -0300

    adicionando metodo update e delete

--- src/main/java/com/example/carros/api/CarrosController.java
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -3,9 +3,11 @@ package com.example.carros.api;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +39,22 @@ public class CarrosController {
 	
 	@PostMapping
 	public String post(@RequestBody Carro carro) {
-		Carro c = service.save(carro);
+		Carro c = service.insert(carro);
 		return "Carro salvo com sucesso: " + c.getId();
+	}
+	
+	@PutMapping("/{id}")
+	public String put(@PathVariable("id") Long id, @RequestBody Carro carro) {
+		Carro c = service.update(carro, id);
+		
+		return "Carro atualizado com sucesso: " + c.getId();
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		service.delete(id);
+		
+		return "Carro deletado com sucesso";
 	}
 
 }
--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class CarroService {
@@ -23,8 +24,40 @@ public class CarroService {
 		return repository.findByTipo(tipo);
 	}
 
-	public Carro save(Carro carro) {
+	public Carro insert(Carro carro) {
+		Assert.isNull(carro.getId(), "Não foi possivel inserir o registro");
+		
 		return repository.save(carro);
+	}
+
+	public Carro update(Carro carro, Long id) {
+		Assert.notNull(id, "Não foi possivel atualizar o registro");
+		
+		//Busca o carro no banco de dados
+		Optional<Carro> optional = getCarroById(id);
+		if(optional.isPresent()) {
+			Carro db = optional.get();
+			// Copiar as propriedades
+			db.setNome(carro.getNome());
+			db.setTipo(carro.getTipo());
+			System.out.println("Carro id " + db.getId());
+			
+			// Atualiza o carro
+			repository.save(db);
+			
+			return db;
+		} else {
+			throw new RuntimeException("Não foi possivel atualizar o registro");
+		}
+		
+	}
+
+	public void delete(Long id) {
+		Optional<Carro> carro = getCarroById(id);
+		if(carro.isPresent()) {
+			repository.deleteById(id);
+		}
+		
 	}
 
 }

2d77b73586e05dbb2e7dd4544ab0c6f4a0701f7e
Jonatas Prates <jonatasluisprates@gmail.com>
2021-05-05 21:09:12 -0300

    Aprendendo adicionar parametros @RequestParam no metodo get no webService

--- src/main/java/com/example/carros/api/IndexController.java
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -1,10 +1,8 @@
 package com.example.carros.api;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,19 +14,10 @@ public class IndexController {
 		return "get spring boot";
 	}
 	
-	@PostMapping
-	public String post() {
-		return "post spring boot";
+	@GetMapping("/nomeCompleto")
+	public String nome(@RequestParam("nome") String nome, @RequestParam("sobrenome") String sobrenome) {
+		return "Nome:" + nome + " Sobrenome:" + sobrenome;
 	}
 	
-	@PutMapping
-	public String put() {
-		return "put spring boot";
-	}
 	
-	@DeleteMapping
-	public String delete() {
-		return "delete spring boot";
-	}
-
 }
